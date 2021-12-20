e54c945eafc3de299ef9173bb83bd61f045fe968
youmari <youmari300@gmail.com>
2021-12-20 20:37:07 +0100

    Add renderCard function to display cards with their data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './css/style.css';
+import { getData } from './js/api';
+import { createCardItem } from './js/createCardItem';
+
+
+const renderItems = async () => {
+  const data = await getData()
+  data.forEach((item,index) => createCardItem(item))
+}
+
+renderItems()
