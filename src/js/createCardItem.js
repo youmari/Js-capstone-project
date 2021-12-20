71f5fac53a93bd485f539c6b458e9c728ffc4727
youmari <youmari300@gmail.com>
2021-12-20 23:31:43 +0100

    Create likes elements

--- src/js/createCardItem.js
+++ src/js/createCardItem.js
@@ -1,21 +1,25 @@
 const cardContainer = document.querySelector('.grid-cards-container');
 
-const createCardItem = (item) => {
+const createCardItem = (item, itemLike = { likes: 0 }) => {
   const cardElement = document.createElement('article');
   const cardImage = document.createElement('img');
   const cardTitle = document.createElement('h4');
   const commentAndLikeDiv = document.createElement('div');
   const commentBtn = document.createElement('button');
+  const pLikesTag = document.createElement('p');
 
+  cardElement.id = item.id;
   cardContainer.append(cardElement);
   cardElement.append(cardImage, cardTitle, commentAndLikeDiv);
   cardElement.classList.add('item');
   cardTitle.classList.add('card-title');
-  commentAndLikeDiv.append(commentBtn);
+  commentAndLikeDiv.append(pLikesTag, commentBtn);
+  pLikesTag.classList.add('p-like');
   commentAndLikeDiv.classList.add('comment-and-like');
   commentBtn.classList.add('comment-btn');
   commentBtn.textContent = 'Comment';
 
+  pLikesTag.textContent = ` ${itemLike.likes} likes`;
   cardImage.src = item.image.medium;
   cardTitle.textContent = item.name;
 };
