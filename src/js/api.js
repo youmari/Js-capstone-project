62dddce5bd05e99a50f6d226682b6fbe6718e4fe
youmari <youmari300@gmail.com>
2021-12-20 23:30:32 +0100

    Add function to get likes form the API

--- src/js/api.js
+++ src/js/api.js
@@ -1,9 +1,14 @@
-const url = 'https://api.tvmaze.com/shows';
+const url1 = 'https://api.tvmaze.com/shows';
+const url2 = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/YtXRS6uhi4iAGPXZph1T/likes';
 
-const getData = () => {
+const fetchData = (url) => {
   const response = fetch(url)
     .then((res) => res.json());
   return response;
 };
+
+const getData = () => fetchData(url1);
+
+export const getLikes = () => fetchData(url2);
 
 export default getData;
