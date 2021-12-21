135a3bc1f0c68cb5e1bd065a175a12208c34b9a8
youmari <youmari300@gmail.com>
2021-12-21 11:07:30 +0100

    Fix the bug and Display the likes the properly

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ document.addEventListener('click', async (e) => {
   if (e.target.matches('.heart')) {
     e.target.classList.toggle('is-active');
     const item_id = Number(e.target.id)
-     addLikes(item_id);
-    //  clearAllElement();
-    //  renderItems();
+     await addLikes(item_id);
+     clearAllElement();
+     await renderItems();
   }
 })
 
@@ -20,9 +20,8 @@ const renderItems = async () => {
   let index = 0;
   const itemsData = await getData();
   const likesData = await getLikes();
+  likesData.sort((a,b) =>  a.item_id - b.item_id)
   console.log(likesData)
-  let data = likesData.sort((a,b) =>  a - b)
-  console.log(data)
   for (let i = 0; i < itemsData.length; i++) {
     if (likesData[index] !== undefined && itemsData[i].id == likesData[index].item_id) {
       createCardItem(itemsData[i],likesData[index])  
