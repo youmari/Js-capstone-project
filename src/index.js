1ed9352429b08b8df1ccd1cde46dd38b0ecab511
youmari <youmari300@gmail.com>
2021-12-21 12:00:38 +0100

    Add likes to the items and update it

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
-import clearAllElement from './clearAllElement';
+/* eslint-disable */
 import './css/style.css';
 import getData, { addLikes, getLikes } from './js/api';
 import createCardItem from './js/createCardItem';
-
 
 document.addEventListener('click', async (e) => {
   if (e.target.matches('.heart')) {
     e.target.classList.toggle('is-active');
     const item_id = Number(e.target.id)
-     await addLikes(item_id);
-     clearAllElement();
-     await renderItems();
+    await addLikes(item_id);
+    let like = Number(e.target.nextSibling.textContent.match(/[0-9]/g).join(''));
+    e.target.nextSibling.textContent = `${like + 1} likes`;
   }
 })
-
-
 
 const renderItems = async () => {
   let index = 0;
   const itemsData = await getData();
   const likesData = await getLikes();
   likesData.sort((a,b) =>  a.item_id - b.item_id)
-  console.log(likesData)
   for (let i = 0; i < itemsData.length; i++) {
     if (likesData[index] !== undefined && itemsData[i].id == likesData[index].item_id) {
       createCardItem(itemsData[i],likesData[index])  
       index++;
     }else {
-      createCardItem(itemsData[i])
+      createCardItem(itemsData[i]);
     }
   }
 }    
-    
-
 
 renderItems();
