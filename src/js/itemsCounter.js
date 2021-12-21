7a6914b6cb537a776ad26524061e299e5e820692
youmari <youmari300@gmail.com>
2021-12-21 14:47:27 +0100

    Fix eslint errors

--- src/js/itemsCounter.js
+++ src/js/itemsCounter.js
@@ -1,9 +1,9 @@
 const TvShowLink = document.querySelector('.tv-show-link');
-const itemsCounter = (data) => data.length
+const itemsCounter = (data) => data.length;
 
 const displayTvShownumbers = (data) => {
-    const counter = itemsCounter(data);
-    TvShowLink.textContent = `TvShows(${counter})`
-}
+  const counter = itemsCounter(data);
+  TvShowLink.textContent = `TvShows(${counter})`;
+};
 
 export default displayTvShownumbers;
