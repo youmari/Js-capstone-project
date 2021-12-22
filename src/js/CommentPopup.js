2d7aa4ec817a270dc1f48f5b8f459244e6d53f0b
KayLemba <kaylemba@yahoo.com>
2021-12-22 18:50:20 +0200

    Refactor fetch code blocks

--- src/js/CommentPopup.js
+++ src/js/CommentPopup.js
@@ -1,18 +1,17 @@
 const url1 = 'https://api.tvmaze.com/shows';
 const commentsEndpoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8v2YvLQLsPQiil6nHJBM/comments';
 const popup = document.querySelector('.movie-popup');
+
+const get = (url) => fetch(url)
+  .then((res) => res.json())
+  .then((data) => data)
+  .then((error) => error);
 const fetchMovieData = async (movieId) => {
-  const response = await fetch(`${url1}/${movieId}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .then((error) => error);
+  const response = await get(`${url1}/${movieId}`);
   return response;
 };
 const fetchMovieComments = async (movieId) => {
-  const response = await fetch(`${commentsEndpoint}?item_id=${movieId}`)
-    .then((res) => res.json())
-    .then((data) => data)
-    .then((error) => error);
+  const response = await get(`${commentsEndpoint}?item_id=${movieId}`);
   return response;
 };
 const displayMovieComments = (data) => {
