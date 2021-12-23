29544fa9e373c1bac581fc4939aec897b3d2cac6
KayLemba <kaylemba@yahoo.com>
2021-12-23 12:40:44 +0200

    Implement add comment functionality

--- src/js/CommentPopup.js
+++ src/js/CommentPopup.js
@@ -5,7 +5,24 @@ const popup = document.querySelector('.movie-popup');
 const get = (url) => fetch(url)
   .then((res) => res.json())
   .then((data) => data)
-  .then((error) => error);
+  .catch((error) => error);
+
+const post = (url, params = {}) => fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(params),
+}).then((res) => res.text())
+  .then((data) => (data.error
+    ? { error: true, info: data }
+    : { error: false, info: data }))
+  .catch((error) => ({ error: true, info: error }));
+
+const addComment = async (params) => {
+  const response = await post(commentsEndpoint, params);
+  return response;
+};
 const fetchMovieData = async (movieId) => {
   const response = await get(`${url1}/${movieId}`);
   return response;
@@ -21,6 +38,19 @@ const enableClosePopup = () => {
   document.querySelector('#close-popup').addEventListener('click', () => {
     popup.style.display = 'none';
     popup.innerHTML = '';
+  });
+};
+const showComments = (movieId) => {
+  fetchMovieComments(movieId).then((data) => {
+    if (!data.error) {
+      let comments = '';
+      data.forEach((comment) => {
+        comments += `<li>${comment.creation_date} ${comment.username}: ${comment.comment}</li>`;
+      });
+      displayMovieComments(comments);
+    } else {
+      displayMovieComments('No comments have been posted yet. Be the first to write something...');
+    }
   });
 };
 const displayMoviePopup = (movieId) => {
@@ -49,24 +79,34 @@ const displayMoviePopup = (movieId) => {
         </td>
       </tr>
     </table>
-    <h3>
+    <h3 class="comment-title">
     Comments (<span class="total-comments">0</span>)
     </h3>
     <ul class="comments">
       fetching comments...
     </ul>
+    <form class="com-form">
+      <h2>Add a comment</h2>
+      <input type="text" name="username" placeholder="Your name">
+      <textarea placeholder="Your insights" name="comment"></textarea>
+      <button type="submit">Submit</button>
+    </form>
     `;
     enableClosePopup();
-    fetchMovieComments().then((data) => {
-      if (!data.error) {
-        let comments = '';
-        data.forEach((comment) => {
-          comments += `<li>${comment.creation_date} ${comment.username}: ${comment.comment}</li>`;
-        });
-        displayMovieComments(comments);
-      } else {
-        displayMovieComments('No comments have been posted yet. Be the first to write something...');
-      }
+    showComments(movieId);
+    const form = popup.querySelector('.com-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const user = form.elements.username.value;
+      const msg = form.elements.comment.value;
+      addComment({
+        item_id: movieId,
+        username: user,
+        comment: msg,
+      }).then(() => {
+        showComments(movieId);
+        form.reset();
+      });
     });
   });
   popup.style.display = 'block';
